27424c6cc3fafe460c7d1fe10fa1eea86366a327
Matheus Vieira Honorio de Souza <matheusvhs@gmail.com>
2019-04-12 00:06:50 -0300

    Create 1001.cpp

---
+++ 1001.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	float a, b, x;
+	
+	cin>> a;
+	cin>> b;
+	
+	x = a + b;
+	cout<< "X = "<< x<< endl;
+	
+	
+	
+	return 0;
+}

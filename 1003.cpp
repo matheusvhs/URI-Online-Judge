c51d57ce491df32f81c84af2ba7775677f76b8a4
Matheus Vieira Honorio de Souza <matheusvhs@gmail.com>
2019-04-12 00:08:44 -0300

    Create 1003.cpp

---
+++ 1003.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	float a, b;
+	
+	cin>> a;
+	cin>> b;
+	
+	cout<< "SOMA = "<<a + b<<endl;
+	
+	
+	
+	return 0;
+}
